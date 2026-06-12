08f4d6fbda8ad78ab83abe26b2fc98bdafafe0de
Turtlerock0010 <danielrprincipe@gmail.com>
2026-06-12 17:50:32 -0400

    Create GPIO_Check.ino with basic structure
    Initialized GPIO_Check.ino with setup and loop functions.

--- examples/GPIO_Check/GPIO_Check.ino
+++ examples/GPIO_Check/GPIO_Check.ino
@@ -1,1 +1,9 @@
-// Nothing here, but soon something will be! Just you wait, just you waiiiiittttt.
+#include <MtN1_Lib.h> // Replace with your actual library header file
+
+void setup() {
+  // Put your setup code here, to run once:
+}
+
+void loop() {
+  // Put your main code here, to run repeatedly:
+}
